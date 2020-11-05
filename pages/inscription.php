8f7ecd43d544fe6b823be464361e76ae0ad5234c
Adam Khalepo <adam.khalepo@iut-rodez.fr>
2020-11-05 11:58:54 +0100

    Ajout de la page inscription

---
+++ pages/inscription.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<HTML lang="fr">
+	<HEAD>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css"/>
+		<link href="../css/style.css" rel="stylesheet"/>
+		<TITLE> Inscription </TITLE>
+	</HEAD>
+	<BODY>
+		
+		<?php
+
+		?>
+
+	</BODY>
+</HTML>
