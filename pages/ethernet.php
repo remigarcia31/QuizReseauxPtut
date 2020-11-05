849a51eb283723c83cee27339cc65eb73e1b7da6
Adam Khalepo <adam.khalepo@iut-rodez.fr>
2020-11-05 11:39:11 +0100

    Création du Git

---
+++ pages/ethernet.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<HTML lang="fr">
+	<HEAD>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css"/>
+		<link href="../css/style.css" rel="stylesheet"/>
+		<TITLE> Quiz Ethernet </TITLE>
+	</HEAD>
+	<BODY>
+	</BODY>
+</HTML>
---
+++ pages/wifi.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<HTML lang="fr">
+	<HEAD>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css"/>
+		<link href="../style.css" rel="stylesheet"/>
+		<TITLE> Quiz Wi-Fi </TITLE>
+	</HEAD>
+	<BODY>
+	</BODY>
+</HTML>
